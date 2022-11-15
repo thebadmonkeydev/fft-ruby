09f7831564ae0a9aeab0859250f6175d0172c28c
Michael Kelly <michaelkelly322@gmail.com>
2022-11-14 23:43:15 -0500

    Make specable

--- spec/fft/ruby_spec.rb
+++ spec/fft/ruby_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Fft::Ruby do
   it "has a version number" do
     expect(Fft::Ruby::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
